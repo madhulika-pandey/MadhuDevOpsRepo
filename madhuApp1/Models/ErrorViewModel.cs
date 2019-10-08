286f1fbcc85e9e0f2d58670936b44a17e2ada3b7
Madhulika Pandey <madhulika0301@gmail.com>
2019-10-08 19:39:05 +1100

    Commit1

---
+++ madhuApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace madhuApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
